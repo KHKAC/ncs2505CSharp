ba6447c0bdb1aef6b21babbff58b90c9a54b5c37
KHKAC <rudgnsrb888@naver.com>
2025-07-07 13:16:25 +0900

    first

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //문제풀이 250704
+        //Console.WriteLine(Soultion0704(3, 2));
+        //문제풀이 250707
+        Console.WriteLine(Soultion0707(99, 6));
+    }
+
+    public static int Soultion0707(int num1, int num2)
+    {
+        int answer = 0;
+        //int answer = num1 - num2;
+        //return answer;
+        return answer;
+    }
+
+    public static int Soultion0704(int num1, int num2)
+    {
+        int answer = num1 % num2;
+        return answer;
+    }
+
+}
