e9310df662a89c9b434d5fd966fd0126ea591af7
KHKAC <rudgnsrb888@naver.com>
2025-07-07 14:01:20 +0900

    두 수의 차

--- Program.cs
+++ Program.cs
@@ -4,15 +4,14 @@
     {
         //문제풀이 250704
         //Console.WriteLine(Soultion0704(3, 2));
+
         //문제풀이 250707
-        Console.WriteLine(Soultion0707(99, 6));
+        Console.WriteLine(Soultion0707(100, 2));
     }
 
     public static int Soultion0707(int num1, int num2)
     {
-        int answer = 0;
-        //int answer = num1 - num2;
-        //return answer;
+        int answer = num1 - num2;
         return answer;
     }
 
