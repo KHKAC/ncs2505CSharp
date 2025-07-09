85ab63b700122722845635a39512c037641432f5
KHKAC <rudgnsrb888@naver.com>
2025-07-09 13:30:29 +0900

    250709 두 수의 나눗셈

--- Program.cs
+++ Program.cs
@@ -9,8 +9,8 @@
         ProgramSolution sol = new ProgramSolution();
         CSharpStudy cSharpStudy = new CSharpStudy();
         //sol.Soultion07082(7, 7);
-        Console.WriteLine(sol.Soultion07082(11, 11));
-        cSharpStudy.DataType();
+        //cSharpStudy.DataType();
+        Console.WriteLine(sol.Soultion07092(1, 16));
     }
     
 }
--- ProgramSolution.cs
+++ ProgramSolution.cs
@@ -1,11 +1,43 @@
 class ProgramSolution
 {
     /// <summary>
+    /// 두 수의 나눗셈
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
+    public int Soultion07092(int num1, int num2)
+    {
+        int answer = 0;
+        // float fnum1 = (float)num1;
+        // float fnum2 = (float)num2;
+        // float fanswer = 0f;
+        // fanswer = fnum1 / fnum2;
+        // fanswer *= 1000f
+
+        float temp = (float)num1 / num2 * 1000f;
+        answer = (int)temp;
+        return answer;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="age"></param>
+    /// <returns></returns>
+    public int Soultion0709(int age)
+    {
+        int nowYear = 2022;
+        int answer = nowYear - age + 1;
+        return answer;
+    }
+
+    /// <summary>
     /// 숫자 비교하기
     /// </summary>
     /// <param name="num1"></param>
     /// <param name="num2"></param>
-    /// <returns> 리턴 값 </returns>
+    /// <returns></returns>
     public int Soultion07082(int num1, int num2)
     {
         if (num1 == num2)
